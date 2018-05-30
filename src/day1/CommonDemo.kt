697d2156fa4ec482e887cc0f01420e147a83f6d3
xiaweizi <1012126908@qq.com>
2018-05-30 18:10:41 +0800

    [dev] 初次提交

---
+++ src/day1/CommonDemo.kt
@@ -0,0 +1,15 @@
+package day1
+
+/**
+ * <pre>
+ *     author : xiaweizi
+ *     class  : day1.CommonDemo
+ *     e-mail : 1012126908@qq.com
+ *     time   : 2018/05/30
+ *     desc   :
+ * </pre>
+ */
+
+fun main(args: Array<String>) {
+    print("this is main method")
+}
