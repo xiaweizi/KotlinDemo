fdfcdc5fa114a01473f7c52e40c9694e37713718
xiaweizi <1012126908@qq.com>
2018-06-02 12:26:24 +0800

    [dev] 添加返回测试代码

---
+++ src/day3/Test.kt
@@ -0,0 +1,23 @@
+package day3
+
+/**
+ * <pre>
+ *     author : xiaweizi
+ *     class  : day3.Test
+ *     e-mail : 1012126908@qq.com
+ *     time   : 2018/06/02
+ *     desc   :
+ * </pre>
+ */
+
+fun main(args: Array<String>) {
+    loop@ for (i in 1..10) {
+        for (j in 11..20) {
+            if (j % 2 == 0) {
+                println("i=$i, j=$j")
+                break@loop
+            }
+            println("---i=$i, j=$j---")
+        }
+    }
+}
